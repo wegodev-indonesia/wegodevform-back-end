da503589053ca97ddf1040a7009daa6be8975b90
Adam Alfiansyah <adamalfiansyah@gmail.com>
2022-09-25 11:47:45 +0700

    Faker hapus email

--- faker/answers.js
+++ faker/answers.js
@@ -7,7 +7,6 @@ const run = async (limit) => {
         for (var i = 0; i < limit; i++) {
             data.push({
                 '6321497603df722a27b35600': faker.name.fullName(),
-                '6321499a03df722a27b3560d': faker.internet.email(),
                 '632149b303df722a27b35613': faker.helpers.arrayElement(['Pria', 'Wanita']),
                 '6324b3bd362aba63469a49fb': faker.helpers.arrayElements(['Dendeng', 'Rendang', 'Semur', 'Nasi Uduk', 'Bubur Ayam']),
                 '6326d2f8652595441e17417b': faker.helpers.arrayElement(['38', '39', '40', '41', '41', '42', '43']),
